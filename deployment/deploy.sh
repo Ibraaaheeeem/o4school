a5d80a8e1e1320ef7a69057515998139f06cb3a7
ibraaaheeeem <ibraaaheeeem@gmail.com>
2026-01-18 23:19:25 +0100

    Fix deploy script to use correct directory and docker compose command

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Navigate to the directory where this script (and docker-compose.yml) is located
+cd "$(dirname "$0")"
+
 # Pull the latest image
 echo "Pulling latest image..."
-docker-compose pull app
+docker compose pull app
 
 # Restart the containers
 echo "Restarting containers..."
-docker-compose up -d
+docker compose up -d
 
 echo "Deployment complete!"
