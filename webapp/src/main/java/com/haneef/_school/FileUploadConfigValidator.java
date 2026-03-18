f28fb1858e04b754bf039e038301baf93100311f
Ibrahim Oyeboade <ibraaaheeeem@gmail.com>
2026-03-18 21:11:14 +0100

    Add FileUploadConfigValidator for validating file upload directory

---
+++ webapp/src/main/java/com/haneef/_school/FileUploadConfigValidator.java
@@ -0,0 +1,7 @@
+// Validate file upload directory
+public class FileUploadConfigValidator {
+    public boolean validateDirectory(String directoryPath) {
+        // Validation logic here
+        return true;
+    }
+}
